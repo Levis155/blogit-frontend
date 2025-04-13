b6dc7ad2484f55fb5a02d832119e9289ebc313dd
Levis155 <levismbui@gmail.com>
2025-04-13 13:51:31 +0300

    implement create blog functionality

--- src/Components/WriteSection/WriteSection.jsx
+++ src/Components/WriteSection/WriteSection.jsx
@@ -1,14 +1,54 @@
 import "./WriteSection.css"
 import ImageUploader from "../ImageUploader/ImageUploader";
+import { useState } from "react";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import { Alert } from "@mui/material";
 
 function WriteSection() {
+  const [title, setTitle] = useState("");
+  const [excerpt, setExcerpt] = useState("");
+  const [formError, setFormError] = useState(null)
+
+  const {isPending, mutate} = useMutation({
+    mutationKey: [],
+    mutationFn: async () => {
+      const response = await axios.post("http://localhost:3000/blogs", {title, excerpt}, {withCredentials: true})
+      return response.data;
+    },
+    onSuccess: (data) => {
+      //redirect to page showing article
+    },
+    onError: (err) => {
+      if(axios.isAxiosError(err)){
+        const serverMessage = err.response.data.message;
+        setFormError(serverMessage);
+      } else {
+        setFormError("Something went wrong.")
+      }
+    },
+  })
+
+  function handlePublish(e) {
+    e.preventDefault();
+    setFormError(null);
+    if(!title || !excerpt) {
+      setFormError("All fields are required.");
+      return;
+    }
+    mutate();
+  }
+  
   return (
     <section className="write-section">
-      <form action="" className="write-form">
+      <form action="" className="write-form" onSubmit={handlePublish}>
         <p className="write-form-title">write your thoughts!</p>
+        {formError && <Alert severity="error" sx={{mb:"1rem", fontSize:"1.4rem"}}>{formError}</Alert>}
         <ImageUploader />
-        <TitleInput label="blog title" inputType="text" id="titleInput" placeHolder="Provide a title to your blog. Try and make it relatively short and catchy."/>
-        <ExcerptInput label="blog excerpt" id="excerpt-input" placeHolder="Provide a brief excerpt to the blog. Give a comprehensive but brief preview to your blog." />
+        <TitleInput value={title} onChange={e => setTitle(e.target.value)} label="blog title" inputType="text" id="titleInput" placeHolder="Provide a title to your blog. Try and make it relatively short and catchy."/>
+        <ExcerptInput value={excerpt} onChange={e => setExcerpt(e.target.value)} label="blog excerpt" id="excerpt-input" placeHolder="Provide a brief excerpt to the blog. Give a comprehensive but brief preview to your blog." />
+
+        <button disabled={isPending} className="publish-btn">{isPending ? "please wait" : "publish"}</button>
       </form>
     </section>
   )
@@ -18,23 +58,27 @@ function TitleInput({
     label,
     inputType,
     id,
+    value,
+    onChange,
     placeHolder,}) {
     return (
       <div className="title-input">
         <label>{label}</label>
-        <input type={inputType} id={id} placeholder={placeHolder} />
+        <input type={inputType} id={id} placeholder={placeHolder} value={value} onChange={onChange} />
       </div>
     );
   }
 
   function ExcerptInput({
     label,
+    value,
+    onChange,
     id,
     placeHolder,}) {
     return (
       <div className="excerpt-input">
         <label>{label}</label>
-        <textarea id={id} placeholder={placeHolder} />
+        <textarea value={value} onChange={onChange} id={id} placeholder={placeHolder} />
       </div>
     );
   }
